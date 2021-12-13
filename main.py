5044147320335410f9c8e4b5283c8ab787681529
colintravisbrown <54756825+colintravisbrown@users.noreply.github.com>
2021-12-13 04:25:35 -0800

    AAAAAAAhhhhhhh

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-from scipy.signal import convolve2d
+from LPythranTest import laplace_pyth
+
 def updateConv(mGrid):
-    ep, f, q, d1, d2 = (0.03, 2.0, 0.002, 1.0, 0.6)
 
-    grid = np.zeros_like(mGrid)
-    mean = np.zeros_like(mGrid)
+    if wrap:
+        l0 = laplace_pyth(mGrid[0], wrap)
+        l1 = laplace_pyth(mGrid[1], wrap)
+    else:
+        l0 = laplace_pyth(np.pad(mGrid[0],1), wrap)/(0.1**2)
+        l1 = laplace_pyth(np.pad(mGrid[1],1), wrap)/(0.1**2)
 
-    kernel_avg = np.array([[1, 1, 1],
-                           [1, 1, 1],
-                           [1, 1, 1]])/9
 
-    mean[0] = convolve2d(mGrid[0], kernel_avg, mode='same', boundary='fill')
-    mean[1] = convolve2d(mGrid[1], kernel_avg, mode='same', boundary='fill')
-
-    l = (mean - mGrid) / (0.1) ** 2
 
     x = mGrid[0]
     y = mGrid[1]
+    mGrid[0] = mGrid[0] + 0.0025 * (d1 * l0 + x + q*f*y/(q + x) - x**2 - x*f*y/(q + x))
+    mGrid[1] = mGrid[1] + 0.0025 * (d2 * l1 + x - y)
 
-    grid[0] = mGrid[0] + 0.0001 * (d1 * l[0] + (1 / ep) * (x * (1 - x) - f * (x - q) / (q + x) * y))
-    grid[1] = mGrid[1] + 0.0001 * (d2 * l[1] + x - y)
-
-    return grid
+    return mGrid
 
 
 def init_spiral(n_grid):
-    ep, f, q, d1, d2 = (0.03, 2.0, 0.002, 1.0, 0.6)
     grid = np.zeros((2,n_grid, n_grid))
-    ss = q*(f + 1)/(f - 1)
+    ss = q*(f+1)/(f-1)
 
     for i in range(n_grid):
         for j in range(n_grid):
@@ -45,9 +44,8 @@ def init_spiral(n_grid):
     return grid
 
 def init_circle(n_grid):
-    ep, f, q, d1, d2 = (0.03, 2.0, 0.002, 1.0, 0.6)
     grid = np.zeros((2,n_grid, n_grid))
-    ss = q*(f + 1)/(f - 1)
+    ss = q*(f+1)/(f-1)
 
     for i in range(n_grid):
         for j in range(n_grid):
@@ -69,35 +67,43 @@ def init_circle(n_grid):
 
 
 
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
 
 mpl.use('TkAgg')
 
-fig = plt.figure()
-
-n_grid = 200
-
-grid = init_circle(n_grid)
+n_grid = 128
+fig, ax = plt.subplots()
 
 
+ep, f, q, d1, d2 = (1, 0.6, 0.0008, 1, 75)
+wrap = True
+ss = 1/2*(1 - f - q + np.sqrt((1 - f - q)**2 + 4*q*(1 + f)))
+grid = np.ones((2,n_grid,n_grid))*ss + (np.random.rand(2,n_grid,n_grid)-0.5)*ss*0.1
+#grid = init_circle(n_grid)
+#grid = init_spiral(n_grid)
+
+for i in range(10000):
+    grid = updateConv(grid)
 
 
-
-im = plt.imshow(grid[0], animated=True, interpolation='None')
-
+im = ax.imshow(grid[0], animated=True)
+# cb = fig.colorbar(im)
 
 def loop2(*args):
     global grid
-    for i in range(100):
+    for i in range(1500):
         grid = updateConv(grid)
     im.set_array(grid[0])
+    # norm = mpl.colors.Normalize(vmin = grid[0].min(), vmax = grid[0].max())
+    # im.set_norm(norm)
+    #im.set_clim(grid[0].min(), grid[0].max())
+    if np.any(grid <0):
+        print('instability')
+        quit()
     return im,
 
 
-ani = animation.FuncAnimation(fig, loop2, blit=True)
+ani = animation.FuncAnimation(fig, loop2, blit=False, frames = 200)
 plt.show()
 # mywriter = animation.FFMpegWriter(fps=30)
 # ani.save('eh.mp4',writer=mywriter)
